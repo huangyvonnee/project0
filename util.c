194d506230cfe3e290e0839f55ebb82c768b2111
masif96 <its.mohammad96@utexas.edu>
2017-02-10 17:44:10 -0600

    Update util.c

--- util.c
+++ util.c
@@ -27,41 +27,41 @@ int parseline(const char *cmdline, char **argv)
     strcpy(buf, cmdline);
     buf[strlen(buf)-1] = ' ';  /* replace trailing '\n' with space */
     while (*buf && (*buf == ' ')) /* ignore leading spaces */
-	buf++;
+    buf++;
 
     /* Build the argv list */
     argc = 0;
     if (*buf == '\'') {
-	buf++;
-	delim = strchr(buf, '\'');
+    buf++;
+    delim = strchr(buf, '\'');
     }
     else {
-	delim = strchr(buf, ' ');
+    delim = strchr(buf, ' ');
     }
 
     while (delim) {
-	argv[argc++] = buf;
-	*delim = '\0';
-	buf = delim + 1;
-	while (*buf && (*buf == ' ')) /* ignore spaces */
-	       buf++;
+    argv[argc++] = buf;
+    *delim = '\0';
+    buf = delim + 1;
+    while (*buf && (*buf == ' ')) /* ignore spaces */
+           buf++;
 
-	if (*buf == '\'') {
-	    buf++;
-	    delim = strchr(buf, '\'');
-	}
-	else {
-	    delim = strchr(buf, ' ');
-	}
+    if (*buf == '\'') {
+        buf++;
+        delim = strchr(buf, '\'');
+    }
+    else {
+        delim = strchr(buf, ' ');
+    }
     }
     argv[argc] = NULL;
     
     if (argc == 0)  /* ignore blank line */
-	return 1;
+    return 1;
 
     /* should the job run in the background? */
     if ((bg = (*argv[argc-1] == '&')) != 0) {
-	argv[--argc] = NULL;
+    argv[--argc] = NULL;
     }
     return bg;
 }
@@ -96,18 +96,8 @@ handler_t *Signal(int signum, handler_t *handler)
     action.sa_flags = SA_RESTART; /* restart syscalls if possible */
 
     if (sigaction(signum, &action, &old_action) < 0)
-	unix_error("Signal error");
+    unix_error("Signal error");
     return (old_action.sa_handler);
-}
-
-//From B&O
-pid_t Fork(void)
-{
-    pid_t pid;
-
-    if ((pid = fork()) < 0)
-        unix_error("Fork error");
-    return pid;
 }
 
 //From string notes posted by Norman
@@ -127,22 +117,96 @@ int startsWith(const char *str, const char *pre)
     return lenstr < lenpre ? 0 : strncmp(pre, str, lenpre) == 0;
 }
 
-void Write(const char* str, int jid, pid_t pid)
+//From B&O
+//Check fork system call
+pid_t Fork(void)
 {
-    ssize_t bytes;
-    const int STDOUT = 1;
-    char buffer[MAXLINE];
+    pid_t pid;
 
-    if(pid == 0 && jid == 0) {
-        bytes = sprintf(buffer, str);
-        write(STDOUT, buffer, bytes);
-    } else if(pid == 0) {
-        bytes = sprintf(buffer, str, jid);
-        write(STDOUT, buffer, bytes);
-    } else if(jid == 0) {
-        bytes = sprintf(buffer, str, pid);
-        write(STDOUT, buffer, bytes);
-    }
-    
+    if ((pid = fork()) < 0)
+        unix_error("Fork error");
+    return pid;
+}
+
+//body of method taken from Project 0 instructions
+//Check write system call
+void Write(char* str, int numBytes) 
+{
+    const int STDOUT = 1;
+    ssize_t bytes = write(STDOUT, str, numBytes);
+    if(bytes != numBytes)
+        unix_error("Write error");
     return;
 }
+
+//Check nanosleep system call
+int Nanosleep(const struct timespec *req, struct timespec *rem)
+{
+    int retval = nanosleep(req, rem);
+    if(errno == EFAULT || errno == EINVAL)
+        unix_error("nanosleep error");
+    return retval;
+}
+
+//Check sigemptyset system call
+void Sigemptyset(sigset_t *mask) 
+{
+    if(sigemptyset(mask) == -1)
+        unix_error("sigemptyset error");
+}
+
+//Check sigaddset system call
+void Sigaddset(sigset_t *mask, int sig)
+{
+    if(sigaddset(mask, sig) == -1)
+        unix_error("sigaddset error");
+}
+
+//Check sigprocmask system call
+void Sigprocmask(int sig, sigset_t *mask, sigset_t *prev)
+{
+    if(sigprocmask(sig, mask, prev) == -1)
+        unix_error("sigprocmask error");
+}
+
+//Check setpgid system call
+void Setpgid(pid_t pid, pid_t pgid)
+{
+    if(setpgid(pid, pgid) == -1)
+        unix_error("setpgid error");
+}
+
+//Check sigsuspend system call
+void Sigsuspend(sigset_t *prev)
+{
+    sigsuspend(prev);
+    if(errno != EINTR)
+        unix_error("sigsuspend error");
+}
+
+//Check kill system call
+void Kill(pid_t pid, int sig)
+{
+    if(kill(-pid, sig) == -1)
+        unix_error("kill error");
+}
+
+//Check sprintf system call
+ssize_t Sprintf(char *buffer, char *str, int jid, pid_t pid)
+{
+   ssize_t bytes = sprintf(buffer, str, jid, pid);
+   if(bytes < 0)
+       unix_error("sprintf error");
+   return bytes;
+}
+
+//Check waitpid system call
+pid_t Waitpid(pid_t pid, int *status, int options)
+{
+   pid_t retval = waitpid(pid, status, options);
+   if(errno != ECHILD && retval < 0)
+       unix_error("waitpid error");
+   return retval;
+}
+
+//Checking execve system call in msh code
