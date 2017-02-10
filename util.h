f38508c7edc6c53117f15fa88e00d7cb184911d4
masif96 <its.mohammad96@utexas.edu>
2017-02-10 17:44:24 -0600

    Update util.h

--- util.h
+++ util.h
@@ -11,9 +11,19 @@ void unix_error(char *msg);
 void app_error(char *msg);
 typedef void handler_t(int);
 handler_t *Signal(int signum, handler_t *handler);
-pid_t Fork(void);
 void substr(char dest[], char src[], int offset, int len);
 int startsWith(const char *str, const char *pre);
-void Write(const char* str, int jid, pid_t pid);
+pid_t Fork(void);
+void Write(char* str, int numBytes);
+int Nanosleep(const struct timespec *req, struct timespec *rem);
+void Sigemptyset(sigset_t *mask);
+void Sigaddset(sigset_t *mask, int sig);
+void Sigprocmask(int sig, sigset_t *mask, sigset_t *prev);
+void Setpgid(pid_t pid, pid_t pgid);
+void Sigsuspend(sigset_t *prev);
+void Kill(pid_t pid, int sig);
+ssize_t Sprintf(char *buffer, char *str, int jid, pid_t pid);
+pid_t Waitpid(pid_t pid, int *status, int options);
+//exec is checked in msh code
 
 #endif
